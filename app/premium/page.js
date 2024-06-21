74b7b11a9bda7a7ace9b141a50cb0338354895d1
Aman--Khare <aman22feb2004@gmail.com>
2024-06-21 18:28:51 +0530

    wrapped useSearchParams in suspense

--- app/premium/page.js
+++ app/premium/page.js
@@ -1,37 +1,11 @@
-"use client";
-
-import styles from "../../styles/premium.module.css";
-import { useSearchParams } from "next/navigation";
-import { db } from "../firebase/firebase";
-import { doc, updateDoc } from "firebase/firestore";
+// pages/payment/index.js
+import { Suspense } from "react";
+import PremiumPage from "./premium";
 
 export default function Premium() {
-  const params = useSearchParams();
-  const uid = params.get("uid");
-
-  function handleBuy() {
-    const docRef = doc(db, "users", uid);
-    updateDoc(docRef, {
-      subscription: "Premium",
-    });
-  }
-
   return (
-    <div className={styles.c}>
-      <div className={styles.card}>
-        <h1 className={styles.phead}>Premium</h1>
-        <h1 className={styles.cost}>₹99.00</h1>
-        <h1 className={styles.fhead}>Features:</h1>
-        <ul className={styles.list}>
-          <li className={styles.item}>Priority Booking</li>
-          <li className={styles.item}>Exclusive Discounts</li>
-          <li className={styles.item}>Flexible Cancellation</li>
-          <li className={styles.item}>Dedicated Support</li>
-        </ul>
-        <button className={styles.buy} onClick={handleBuy}>
-          Buy Now
-        </button>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <PremiumPage />
+    </Suspense>
   );
 }
---
+++ app/premium/premium.js
@@ -0,0 +1,37 @@
+"use client";
+
+import styles from "../../styles/premium.module.css";
+import { useSearchParams } from "next/navigation";
+import { db } from "../firebase/firebase";
+import { doc, updateDoc } from "firebase/firestore";
+
+export default function PremiumPage() {
+  const params = useSearchParams();
+  const uid = params.get("uid");
+
+  function handleBuy() {
+    const docRef = doc(db, "users", uid);
+    updateDoc(docRef, {
+      subscription: "Premium",
+    });
+  }
+
+  return (
+    <div className={styles.c}>
+      <div className={styles.card}>
+        <h1 className={styles.phead}>Premium</h1>
+        <h1 className={styles.cost}>₹99.00</h1>
+        <h1 className={styles.fhead}>Features:</h1>
+        <ul className={styles.list}>
+          <li className={styles.item}>Priority Booking</li>
+          <li className={styles.item}>Exclusive Discounts</li>
+          <li className={styles.item}>Flexible Cancellation</li>
+          <li className={styles.item}>Dedicated Support</li>
+        </ul>
+        <button className={styles.buy} onClick={handleBuy}>
+          Buy Now
+        </button>
+      </div>
+    </div>
+  );
+}
